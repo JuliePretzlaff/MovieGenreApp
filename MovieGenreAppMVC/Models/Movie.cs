c62e513ad3f67921e4cdf0cbb86f5c3d826f2b9e
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-03-07 10:11:45 -0500

    initial commit

---
+++ MovieGenreAppMVC/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieGenreAppMVC.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Date Released")]
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM/dd}")]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
